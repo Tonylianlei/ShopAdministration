4c2bb8b2a2902aeacf6446c070d3e9c075115afb
tony lian <tony.lian@chinaventure.com.cn>
2018-01-11 11:19:43 +0800

    添加spring配置文件，成功启动

--- src/main/java/com/tony/shopping/controller/BaseController.java
+++ src/main/java/com/tony/shopping/controller/BaseController.java
@@ -1,9 +1,12 @@
 package com.tony.shopping.controller;
 
+import org.springframework.stereotype.Controller;
+
 /**
  * Created by Tony on 2018/1/10.
  * com.tony.shopping.controller.ShopAdministration
  * 描述：
  */
+@Controller
 public class BaseController {
 }
